c466279d83f39db93cd78cf4e2d8572690b23916
Waqas Wahid <31756914+waqaswahid1@users.noreply.github.com>
2021-01-13 00:14:54 +0500

    Create AirlineTokens.sol

---
+++ AirlineTokens.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC1155/ERC1155.sol";
+
+contract AirlineTokens is ERC1155 {
+    address public governance;
+    uint256 public airlineCount;
+    
+    modifier onlyGovernance() {
+        require(msg.sender == governance, "only governance can call this");
+        
+        _;
+    }
+
+    constructor(address governance_) public ERC1155("") {
+        governance = governance_;
+        airlineCount = 0;
+    }
+    
+    function addNewAirline(uint256 initialSupply) external onlyGovernance {
+        airlineCount++;
+        uint256 airlineTokenClassId = airlineCount;
+
+        _mint(msg.sender, airlineTokenClassId, initialSupply, "");        
+    }
+}
